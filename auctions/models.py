6c5b0e72b15073a9ffc78802d582209cf040e912
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2023-05-25 00:01:08 -0500

    Creating first models in user

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=64, unique=True)
+    email = models.CharField(max_length=64, unique=True)
+    password = models.CharField(max_length=48)
