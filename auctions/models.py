9efd89d4ddbd8695580d9a543989c2758dc30d81
MiguelMR96 <miguelmontoyaramirez96@gmail.com>
2023-07-04 01:04:54 -0500

    Uploading gitignore and some other changes to models

--- auctions/models.py
+++ auctions/models.py
@@ -5,5 +5,19 @@ from django.db import models
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=128, unique=False, default="nn")
     email = models.CharField(max_length=64, unique=True)
     password = models.CharField(max_length=48)
+
+class Listing(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    description = models.JSONField()
+    date = models.DateField()
+
+class Bid(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    listing_id = models.ForeignKey(Listing, on_delete=models.PROTECT)
+    bid = models.IntegerField(null=False)
+    date = models.DateField()
