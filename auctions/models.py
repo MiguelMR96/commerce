004404c9c8a9f92e608140f8be19516bead2bf4c
Miguel Montoya R <miguelmontoyaramirez96@gmail.com>
2023-07-17 19:58:51 -0500

    Updating models

--- auctions/models.py
+++ auctions/models.py
@@ -11,26 +11,34 @@ class User(AbstractUser):
     def __str__(self):
         return f"{self.username} - {self.id}: {self.name} at {self.email}"
 
-class Listing(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=64)
-    image = models.CharField(max_length=256)
-    description = models.JSONField()
-    date = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name="owner")
+class Category(models.Model):
+    name = models.CharField(max_length=64)
 
     def __str__(self):
-        return f"{self.title} - {self.id}: {self.description} at {self.date} of {self.owner}"
+        return f"{self.name}"
 
 class Bid(models.Model):
     id = models.AutoField(primary_key=True)
     bid = models.IntegerField(null=False)
-    listing = models.ForeignKey(Listing, on_delete=models.PROTECT, related_name="item")
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="bidder")
-    date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.id} bid: {self.listing} at {self.bid}"
+
+class Listing(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=64)
+    image_url = models.CharField(max_length=1064)
+    description = models.JSONField()
+    price = models.ForeignKey(Bid, on_delete=models.CASCADE, related_name="bid")
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.id}: {self.description} at {self.date} of {self.owner}"
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
